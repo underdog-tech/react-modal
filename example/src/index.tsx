2b26d8b62f4e68a6caae1efb588b70f3ca9ba4e7
Rene Loperena <reneloperena@live.com>
2022-12-07 10:12:55 -0600

    Remove console.log

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -4,6 +4,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-console.log('hello')
-console.log(document.getElementById('root'))
 ReactDOM.render(<App />, document.getElementById('root'))
