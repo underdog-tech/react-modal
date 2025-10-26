58cea46fcf257db75a8e5df347570b6cc14294db
Allen Gao <allen.gao@pinwheelapi.com>
2025-10-26 12:49:16 -0700

    Add isIntegratedSwitch field to Bill Switch payloads

--- src/client-events/registry/v2.3.ts
+++ src/client-events/registry/v2.3.ts
@@ -94,7 +94,8 @@ export type ScreenTransitionEventPayload = {
 export type BillSwitchPayload = {
   platformId: string
   platformName: string
-  isGuidedSwitch: boolean
+  isGuidedSwitch: boolean // deprecated: will be removed in future version
+  isIntegratedSwitch: boolean
   frequency: string
   nextPaymentDate: string
   amountCents: number
