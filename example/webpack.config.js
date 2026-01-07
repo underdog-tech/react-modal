3741f4c6422b95b18c1886a10118933119377113
Wes Garlock <wes.garlock@pinwheelapi.com>
2026-01-07 14:06:59 -0500

    Setup react sdk to expose CSP.

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,45 +1,49 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '.env') });
-const webpack = require('webpack');
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '.env') })
+const webpack = require('webpack')
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
-  entry: "./src/index.tsx",
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.tsx',
   output: {
-    filename: "bundle.js"
+    filename: 'bundle.js'
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.ts', '.tsx', '.js'],
     extensionAlias: {
-     ".js": [".js", ".ts"],
-     ".cjs": [".cjs", ".cts"],
-     ".mjs": [".mjs", ".mts"]
+      '.js': ['.js', '.ts'],
+      '.cjs': ['.cjs', '.cts'],
+      '.mjs': ['.mjs', '.mts']
     },
     alias: {
-      "@pinwheel/react-modal": path.resolve(__dirname, "../dist")
+      '@pinwheel/react-modal': path.resolve(__dirname, '../dist')
     }
   },
   module: {
     rules: [
-      { test: /\.([cm]?ts|tsx)$/, loader: "ts-loader" },
+      { test: /\.([cm]?ts|tsx)$/, loader: 'ts-loader' },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.PINWHEEL_API_KEY': JSON.stringify(process.env.PINWHEEL_API_KEY || ''),
-      'process.env.PINWHEEL_API_URL': JSON.stringify(process.env.PINWHEEL_API_URL || 'https://sandbox.getpinwheel.com')
+      'process.env.PINWHEEL_API_KEY': JSON.stringify(
+        process.env.PINWHEEL_API_KEY || ''
+      ),
+      'process.env.PINWHEEL_API_URL': JSON.stringify(
+        process.env.PINWHEEL_API_URL || 'https://sandbox.getpinwheel.com'
+      )
     })
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, 'public')
     },
     compress: true,
-    port: 9000,
-  },
-};
+    port: 9000
+  }
+}
